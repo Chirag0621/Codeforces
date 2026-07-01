e22bee51c7c29499e1f86d4018f20b1d15651b1a
Chirag Bhadani <kumarchirag978@gmail.com>
2026-07-02 00:13:16 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+ 
+int main(){
+    long long t;
+    cin >> t;
+ 
+    while(t--){
+        int n , m, i, j;
+        cin >> n >> m >> i >> j;
+ 
+        cout << 1 <<" " << 1 << " " << n << " " << m <<"
+";
+    }
+}
